5d5e62eac748861563bf272c4f403cd15e8fc4ac
Amply <93294146+amplyllc@users.noreply.github.com>
2021-11-22 01:37:02 -0500

    copywriting

--- js/phase.js
+++ js/phase.js
@@ -1,13 +1,15 @@
 function logIn() {
     let x = document.getElementById("password").value;
-    let div = document.querySelector('#incorrect')
+    let form = document.getElementById("login-form");
+    let div = document.getElementById('incorrect')
 
-    if (x === "123") {
-        window.location.replace("https://amply.llc/portal");
-        console.log('correct')
-    } else {
+    if (x != "123") {
         div.className = "show";
         console.log('incorrect');
         return false
+    } else {
+        form.className = "hide";
+        console.log('correct')
+        return true
     }
 }
